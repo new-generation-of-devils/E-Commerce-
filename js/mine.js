f12d9d42d1880efe2c241a081d418767470d31d5
unknown <mmmg998877@gmail.com>
2019-03-03 01:10:11 +0200

    First commit for Home first slider

---
+++ js/mine.js
@@ -0,0 +1,98 @@
+
+
+/*$(window).ready(function(){
+    $(".loading").fadeOut(2000,function(){
+        $("body").css("overflow","auto");
+    })
+})*/
+
+
+
+
+
+var vh = window.innerHeight ;
+var vw =window.innerWidth ;
+
+
+$("#homesec1").css({"height" : vh});
+
+var w = 1920;
+var h = 952;
+
+var p = 24 ; 
+var hone = 100 ; 
+var htwo = 38 ; 
+var hfour = 18 ;
+    
+
+
+
+
+
+
+/*var typed = new Typed("#ty", {
+  // Waits 1000ms after typing "First"
+  strings: ["Devil's Hospital ^2000", "Private Hospital ^2000" ,"Have A Tour ^2000"],
+    
+    loop: true,
+  loopCount: Infinity,
+    
+    typeSpeed: 50
+});
+
+var typed = new Typed("#ty1", {
+  // Waits 1000ms after typing "First"
+  strings: ["Devil's Hospital ^2000", "Private Hospital ^2000" ,"Have A Tour ^2000"],
+    
+    loop: true,
+  loopCount: Infinity,
+    
+    typeSpeed: 50
+});
+
+
+var typed = new Typed("#ty2", {
+  // Waits 1000ms after typing "First"
+  strings: [" medicens ^2000", " treatments ^2000" ," medical devices ^2000"],
+    
+    loop: true,
+  loopCount: Infinity,
+    
+    typeSpeed: 50
+});
+
+
+
+var typed = new Typed("#ty3" ,{
+  // Waits 1000ms after typing "First"
+  strings: [" Our Staff ^2000", " Our Members ^2000" ," Our Doctors ^2000"],
+    
+    loop: true,
+  loopCount: Infinity,
+    
+    typeSpeed: 50
+});
+
+
+var typed = new Typed("#ty4" ,{
+  // Waits 1000ms after typing "First"
+  strings: [" Our Staff ^2000", " Our Members ^2000" ," Our Doctors ^2000"],
+    
+    loop: true,
+  loopCount: Infinity,
+    
+    typeSpeed: 50
+});
+
+
+var typed = new Typed("#ty5" ,{
+  // Waits 1000ms after typing "First"
+  strings: [" Our Staff ^2000", " Our Members ^2000" ," Our Doctors ^2000"],
+    
+    loop: true,
+  loopCount: Infinity,
+    
+    typeSpeed: 50
+});*/
+
+
